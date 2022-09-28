d7a4c0d5f8334436f45cf671524bd318526154a4
Vladik Leshko <leshkovladik@gmail.com>
2022-09-28 15:33:40 +0300

    changet db credentials

--- db.js
+++ db.js
@@ -5,7 +5,8 @@ const pool = new Pool({
     host: 'ec2-34-241-90-235.eu-west-1.compute.amazonaws.com',
     database: 'd81jcf9f4114ml',
     password: '73aa42e519d448f9daf8326a503a088ce4bb4b8c5081db455f311ee47d5affbf',
-    port: 5432.
+    port: 5432,
+    ssl: true,
 });
 
 module.exports = pool;
