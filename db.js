3d41b4d1ab122f8642b0c16809f5a7d52dd9bb8e
Vladik Leshko <leshkovladik@gmail.com>
2022-09-28 12:08:41 +0300

    changed database credentials

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 const Pool = require('pg').Pool;
 
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'wormydb',
-    password: 'root',
+    user: 'hzbscaqtevepky',
+    host: 'ec2-34-241-90-235.eu-west-1.compute.amazonaws.com',
+    database: 'd81jcf9f4114ml',
+    password: '73aa42e519d448f9daf8326a503a088ce4bb4b8c5081db455f311ee47d5affbf',
     port: 5432.
 });
 
